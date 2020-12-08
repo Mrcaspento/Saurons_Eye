660896e5c4b43921f4ab089b2c6eb89f9afe1eac
Caspento <Mrcaspento@gmail.com>
2020-12-08 16:31:04 -0500

    addRole working perfect

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ An Employee tracker app
 Developers are often tasked with creating interfaces that make it easy for non-developers to view and interact with information stored in databases. Often these interfaces are known as **C**ontent **M**anagement **S**ystems. In this homework assignment, your challenge is to architect and build a solution for managing a company's employees using node, inquirer, and MySQL.
 
 ## Instructions
-VALUE
+VALUEnpm uninstall `ls -1 node_modules | tr '/\n' ' '`
 
 Design the following database schema containing three tables:
 
--- suspicously_sticky_sock.js
+++ suspicously_sticky_sock.js
@@ -9,13 +9,10 @@ const connectionProperties = {
     database: "uruk_haitrackerdb"
 }
 
-
-
-
 //CONNECTION
 const connection = mysql.createConnection(connectionProperties);
-connection.connect(function(err){
-    if(err) throw err;
+connection.connect(function (err) {
+    if (err) throw err;
     console.log("connected" + connection.threadId);
 })
 
@@ -67,14 +64,25 @@ middleEarth = () => {
             }
         })
 }
+function addDepartment() {
+    connection.query('SELECT departments, departments.name AS Name', function (err, res) {
+        inquirer
+            .prompt([
+                {
+                    name: 'departments',
+                    type: 'input',
+                    message: "what department would you like to add?"
+                }
+            ]).then(res => {
 
-         
+            })
+    })
+};
 
 function addRole() {
-      
-        connection.query('SELECT role, role.title AS Title, role.powerlvl AS Powerlvl FROM role', function (err, res) {
-            inquirer
-                .prompt([
+    connection.query('SELECT roles, roles.title AS Title, roles.powerlvl AS Powerlvl FROM roles,', function (err, res) {
+        inquirer
+            .prompt([
                 {
                     name: "Title",
                     type: "input",
@@ -83,13 +91,75 @@ function addRole() {
                     name: "Powerlvl",
                     type: "input",
                     message: "Sauron says your PowerLvl is?"
+                }, {
+                    type: "list",
+                    message: "What department do they belong to ?",
+                    name: 'departments',
+                    choices: ["Humans", "Dwarfs", "Elfs", "Sauron"]
+                }
+            ]).then(val => {
+                switch (val.departments) {
+                    case 'Humans':
+                        val.departments = 1;
+                        break;
+                    case 'Dwarfs':
+                        val.departments = 2;
+                        break;
+                    case 'Elfs':
+                        val.departments = 3;
+                        break;
+                    case 'Sauron':
+                        val.departments = 4;
+                        break;
+                }
+                console.log('adding role')
+                connection.query(
+                    "INSERT INTO roles SET ?",
+                    {
+                        title: val.Title,
+                        powerlvl: val.Powerlvl,
+                        department_id: val.Department
+                    },function(err){
+                        if(err) throw err
+                        console.table(val);
+                        middleEarth();
+                    }
+                )
+            })
+    });
+};
+
+function addEmployee() {
+
+    connection.query('SELECT employees, employees.first_name AS FirstName, employees.last_name AS LastName, employees.manager_id AS Manager, employees.role_id AS Role FROM employees', function (err, res) {
+        inquirer
+            .prompt([
+                {
+                    name: "FirstName",
+                    type: "input",
+                    message: "What is this fodders first name?"
+                }, {
+                    name: "LastName",
+                    type: "input",
+                    message: "What is this fodders Last name?"
+                }, {
+                    name: "Manager",
+                    type: "list",
+                    message: "Who is you manager?",
+                    choices: ["",]
+                }, {
+                    name: "Role",
+                    type: "choice",
+                    message: "SAURON ASKS WHAT IS YOUR ROLE?"
                 }
             ]).then(function (res) {
                 connection.query(
-                    "INSERT INTO role SET ?",
+                    "INSERT INTO employees SET ?",
                     {
-                        title: res.Title,
-                        powerlvl: res.Powerlvl,
+                        first_name: res.FirstName,
+                        last_name: res.LastName,
+                        manager_id: res.Manager,
+                        role_id: res.Role
                     },
                     function (err) {
                         if (err) throw err
@@ -98,8 +168,8 @@ function addRole() {
                     }
                 )
             });
-        });
-    };
+    });
+};
 
 
 module.exports = middleEarth();
