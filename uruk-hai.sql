9bb1de0508bb93ed9bbfbc6f0c07aa8ccc4b4abb
Caspento <Mrcaspento@gmail.com>
2020-12-07 19:22:18 -0500

    sql and seeds done populated the server table

--- uruk-hai.sql
+++ uruk-hai.sql
@@ -2,16 +2,16 @@ DROP DATABASE IF EXISTS  uruk_hai_trackerDB;
 CREATE DATABASE uruk_hai_trackerDB;
 USE uruk_hai_trackerDB;
 
---Department--
+-- Department --
 CREATE TABLE department(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30)
 );
 
 CREATE TABLE role(
-    id INIT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
-    powerlvl DECIMAL, --salary--
+    powerlvl DECIMAL, -- salary --
     department_id INT,
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
@@ -21,48 +21,48 @@ CREATE TABLE employee(
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
-    FOREIGN KEY (role_id) REFRENCES  role(id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id)
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
 );
---Department seeds--
+-- Department seeds--
 INSERT INTO department (name)
-VALUE (1,"Humans");
-INSERT INTO
-VALUE (2,"Dwarfs");
-INSERT INTO
-VALUE(3,"Elfs");
-INSERT INTO
-VALUE(4,"Sauron");
+VALUE ("Humans");
+INSERT INTO department (name)
+VALUE ("Dwarfs");
+INSERT INTO department (name)
+VALUE("Elfs");
+INSERT INTO department (name)
+VALUE("Sauron");
 
 
---employee role seeds-- --manager
-INSERT INTO role (title, powerlvl, department_id) --1--
+-- employee role seeds-- -- manager --
+INSERT INTO role (title, powerlvl, department_id) 
 VALUE ("Wizard", 900, 1);
-INSERT INTO role (title, powerlvl, department_id)--2--
+INSERT INTO role (title, powerlvl, department_id)
 VALUE ("Spearmen", 20000, 3);
-INSERT INTO role (title, powerlvl, department_id)--3--
+INSERT INTO role (title, powerlvl, department_id) 
 VALUE ("Elf Archer", 39500, 2);
-INSERT INTO role (title, powerlvl, department_id)--4--
+INSERT INTO role (title, powerlvl, department_id)
 VALUE ("Halfling", 45000, 1);
-INSERT INTO role (title, powerlvl, department_id)--5--
+INSERT INTO role (title, powerlvl, department_id)
 VALUE ("Fodder", 500 , 3);
-INSERT INTO role (title, powerlvl, department_id)--6--
+INSERT INTO role (title, powerlvl, department_id)
 VALUE ("Uruk-hai", 19000, 4);
 
 
---employee seeds
+-- employee seeds --
 
-INSERT INTO role (first_name, last_name, manager_id, role_id)
-VALUE ("Frodo","Baggins", 1, 1);
-INSERT INTO role (first_name, last_name, manager_id, role_id)
-VALUE ("Saurman","Littleman", 4, 2);
-INSERT INTO role (first_name, last_name, manager_id, role_id)
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ( "Frodo", "Baggins", 1, 1);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Saurman","Littleman", NULL, 2);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
 VALUE ("Nazgûl","SnappyDwarf", 2, 3);
-INSERT INTO role (first_name, last_name, manager_id, role_id)
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
 VALUE ("Aragorn","KingOfKings", 1, 4);
-INSERT INTO role (first_name, last_name, manager_id, role_id)
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
 VALUE ("Legolas","Bloom", 3, 5);
-INSERT INTO role (first_name, last_name, manager_id, role_id)
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
 VALUE ("Gandalf","The Grey", 1, 6);
 
 
