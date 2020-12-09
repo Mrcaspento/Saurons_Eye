1a7bd2725fbb753749e0abfd5447968bd64b6d97
Caspento <Mrcaspento@gmail.com>
2020-12-08 20:55:21 -0500

    makingmoves

--- suspicously_sticky_sock.js
+++ suspicously_sticky_sock.js
@@ -26,11 +26,11 @@ middleEarth = () => {
             type: "list",
             message: "what would you like to do?",
             choices: [
-                "View all employees",
-                "View all employees by role", //
-                "View all employees by department",
+                "View all employees",//finished
+                "View all employees by role", //finshed
+                "View all employees by department",//finished
                 "View all employees by manager", //The Nine Nazgûl
-                "Add employee",
+                "Add employee",//finished
                 "Add role",//finished
                 "Add department",//finished
                 "Update employee role"
@@ -61,11 +61,14 @@ middleEarth = () => {
                 case "Add role":
                     addRole();
                     break;
+                case "Update employee role":
+                    updateEmployeeRole();
+                    break;
             }
         })
 }
 function addDepartment() {
-    connection.query('SELECT departments, departments.name AS Name', function (err, res) {
+    connection.query('SELECT departments, departments.name AS Name', function (err, val) {
         inquirer
             .prompt([
                 {
@@ -76,12 +79,12 @@ function addDepartment() {
             ]).then(val => {
                 console.log(`adding ${val.departments} to the table`)
                 connection.query(
-                    "INSERT INTO departments SET ?", 
+                    "INSERT INTO departments SET ?",
                     {
                         name: val.departments
 
-                    }, function(err) {
-                        if(err) throw err;
+                    }, function (err) {
+                        if (err) throw err;
                         console.table(val);
                         console.log('ERROR 404....sike it worked')
                         middleEarth();
@@ -90,23 +93,26 @@ function addDepartment() {
             })
     })
 };
-function viewAllEmployees(){
-    connection.query('SELECT * FROM employees', function(err, val) {
+function viewAllEmployees() {
+    connection.query('SELECT * FROM employees', function (err, val) {
         console.log('\n Employees for middleEarth \n');
         console.table(val);
-    } )
+        middleEarth();
+    })
 }
-function viewAllRoles(){
-    connection.query('SELECT * FROM roles', function(err, val){
+function viewAllRoles() {
+    connection.query('SELECT * FROM roles', function (err, val) {
         console.log('\n Roles for middleEarth \n');
         console.table(val);
+        middleEarth();
     })
-
 }
-function viewAllDepartments(){
-    connection.query('SELECT * FROM departments', function(err, val){
+
+function viewAllDepartments() {
+    connection.query('SELECT * FROM departments', function (err, val) {
         console.log('\n Departments for middleEarth \n');
         console.table(val);
+        middleEarth();
     })
 }
 function addRole() {
@@ -149,8 +155,8 @@ function addRole() {
                         title: val.Title,
                         powerlvl: val.Powerlvl,
                         department_id: val.Department
-                    },function(err){
-                        if(err) throw err
+                    }, function (err) {
+                        if (err) throw err
                         console.table(val);
                         middleEarth();
                     }
@@ -161,7 +167,7 @@ function addRole() {
 
 function addEmployee() {
 
-    connection.query('SELECT employees, employees.first_name AS FirstName, employees.last_name AS LastName, employees.manager_id AS Manager, employees.role_id AS Role FROM employees', function (err, res) {
+    connection.query('SELECT employees, employees.first_name AS FirstName, employees.last_name AS LastName, employees.manager_id AS Manager employees.role_id AS Role FROM employees', function (err, res) {
         inquirer
             .prompt([
                 {
@@ -172,33 +178,112 @@ function addEmployee() {
                     name: "LastName",
                     type: "input",
                     message: "What is this fodders Last name?"
-                }, {
+                },{
                     name: "Manager",
-                    type: "list",
-                    message: "Who is you manager?",
-                }, {
+                    type: "input",
+                    message:"who manages this fool?",
+                    
+                },{
                     name: "Role",
-                    type: "choice",
-                    message: "SAURON ASKS WHAT IS YOUR ROLE?"
+                    type: "list",
+                    message: "SAURON ASKS WHAT IS YOUR ROLE?",
+                    choices: ["Wizard", "Spearmen","Elf Archer", "Halfling", "Fodder", "Uruk-hai"]
                 }
-            ]).then(function (res) {
+            ]).then(val => {
+                console.log('roles being drawn')
+                switch (val.Role){
+                    case 'Wizard':
+                        val.Role = 1;
+                        break;
+                    case 'Spearmen':
+                        val.Role = 2;
+                        break;
+                    case 'Elf Archer':
+                        val.Role = 3;
+                        break;
+                    case 'Halfling':
+                        val.Role = 4;
+                        break;
+                    case 'Fodder':
+                        val.Role = 5;
+                        break;
+                    case 'Uruk-hai':
+                        val.Role = 6;
+                        break;
+                }
                 connection.query(
                     "INSERT INTO employees SET ?",
                     {
-                        first_name: res.FirstName,
-                        last_name: res.LastName,
-                        manager_id: res.Manager,
-                        role_id: res.Role
+                        first_name: val.FirstName,
+                        last_name: val.LastName,
+                        role_id: val.Role,
+                        manager_id: val.Manager
                     },
                     function (err) {
                         if (err) throw err
-                        console.table(res);
+                        console.table(val);
                         middleEarth();
                     }
                 )
             });
     });
 };
+function updateEmployeeRole() {
+    let allemployees = [];
+    connection.query('SELECT * FROM employees', function (err, val) {
+        for (var i = 0; i < val.length; i++) {
+            let updateEmployee =
+                val[i].id + " " + val[i].first_name + " " + val[i].last_name;
+            allemployees.push(updateEmployee)
+            console.log('you made it without any errs');
+        } inquirer
+            .prompt([
+                {
+                    type: "list",
+                    name: "updateEmployeeRole",
+                    message: "Select the someone from the Garrison to update role!",
+                    choices: allemployees
+                },
+                {
+                    type: "list",
+                    name: "newEmployeeRole",
+                    choices: ["Wizard", "Spearmen","Elf Archer", "Halfling", "Fodder", "Uruk-hai"],
+                    message: "**SAURON DEMANDS YOU CHANGE ROLES PICK OR DO NOT PASS!**"
+                }
+            ]).then(val => {
+                console.log('switching roles')
+                switch (val.newEmployeeRole){
+                    case 'Wizard':
+                        val.newEmployeeRole = 1;
+                        break;
+                    case 'Spearmen':
+                        val.newEmployeeRole = 2;
+                        break;
+                    case 'Elf Archer':
+                        val.newEmployeeRole = 3;
+                        break;
+                    case 'Halfling':
+                        val.newEmployeeRole = 4;
+                        break;
+                    case 'Fodder':
+                        val.newEmployeeRole = 5;
+                        break;
+                    case 'Uruk-hai':
+                        val.newEmployeeRole = 6;
+                        break;
+                }
+                console.log(`Changing to ${val.newEmployeeRole} role..\n`);
+                connection.query("UPDATE employees SET role_id = ? WHERE id = ?",function(err){
+                    if(err) throw err
+                    console.table(val);
+                    middleEarth();
+                })
+            }
+            )
+    })
 
+
+
+}
 
 module.exports = middleEarth();
